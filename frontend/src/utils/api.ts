629b7e15496d576de36315b7289499177d34870f
PRATIKSHA KOTE <pratikshakote2002@gmail.com>
2026-07-12 19:36:34 +0530

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://groweasy-csv-importer-54w2.onrender.com';
 
 export async function uploadCSVPreview(file: File) {
   const formData = new FormData();
